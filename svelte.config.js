36570de71c65c154a532e4326623bc6248493ec7
meticoeus <meticoeus@gmail.com>
2023-11-30 10:47:22 -0600

    chore: Testing github page deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,10 +14,15 @@ const config = {
     },
 
     kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter(),
+        adapter: adapter({
+            pages: "build",
+            assets: "build",
+            fallback: null,
+            precompress: false,
+        }),
+        paths: {
+            base: dev ? "" : "/svelte-photoviewer",
+        },
     },
 };
 
